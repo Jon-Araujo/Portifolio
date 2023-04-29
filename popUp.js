2e6f4130fb64d482ad386b4d50d868183e77c2be
Jonathan Araujo <jonathan.araujo76@icloud.com>
2023-04-29 11:38:28 -0300

    PopUp finalizado, consumindo dinâmicamente o arquivo json.

--- popUp.js
+++ popUp.js
@@ -5,19 +5,24 @@ export async function popUpProjetos() {
 
     for (let i = 0; i < cardVerMais.length; i++) {
         cardVerMais[i].insertAdjacentHTML('afterend', `
-            <div class="modal fade" id="exampleModal${i}" tabindex="-1" aria-labelledby="exampleModalLabel${i}" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-                    <div class="modal-content">
+            <div class="modal fade" id="card${i}" tabindex="-1" aria-labelledby="cardLabel${i}" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
+                    <div class="modal-content" style="background-color: var(--cor-cinza)">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel${i}">${listaProjetos[i].titulo}</h1>
+                            <h1 class="modal-title fs-5" id="cardLabel${i}">${listaProjetos[i].titulo}</h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
-                        <div class="modal-body">
-                            ...
+                        <div class="modal-body d-flex">
+                            <video style="width: 60%;" src="${listaProjetos[i].video}" width="440px" autoplay="true"></video>
+                            <div class="d-flex flex-column text-start" style="width: 40%; padding: 0 0.5rem 0 3rem;">
+                                <p class="fs-5 fw-normal">${listaProjetos[i].descricao}</p>
+                                <p class="fs-6">${listaProjetos[i].data}</p>
+                            </div>
                         </div>
                         <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                            <a href="${listaProjetos[i].projeto}" target="_blank"><button type="button" class="btn-link-popUp btn me-4">Projeto</button></a>
+                            <a href="${listaProjetos[i].repositorio}" target="_blank"><button type="button" class="btn-link-popUp btn me-4">Repositório</button></a>
+                            <a href="${listaProjetos[i].linkedin}" target="_blank"><button type="button" class="btn-link-popUp btn">Publicação</button></a>
                         </div>
                     </div>
                 </div>
